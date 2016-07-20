566f2a2fad3218c28ec10a6eb9556e583479a0d3
stvngrcia <stevengarciaacuna@gmail.com>
2016-07-20 10:32:54 -0700

    creating test for the creation of users and checking that their id in the database is the correct one

--- api/tests/user.py
+++ api/tests/user.py
@@ -12,11 +12,13 @@ class userTest(unittest.TestCase):
         # connecting to database
         db.connect()
         # creating a database
-        db.create_tables([User])
+        db.create_tables([User], safe=True)
 
     # def tearDown(self):
         # deleting user
         # User.delete().execute()
+        # deleting the table user
+        # db.drop_tables([User])
 
     def test_create(self):
         # testing the post request for user with all parameters
@@ -24,22 +26,42 @@ class userTest(unittest.TestCase):
                                           last_name='Snow',
                                           email='jon@snow',
                                           password='toto1234'))
+        self.assertEqual(User.select(id), 1)
+
         # testing the post request for user without name parameter
         self.app.post('/users', data=dict(first_name=' ',
                                           last_name='Snow',
                                           email='jon+1@snow',
                                           password='toto1234'))
-        # testing the post request for user without email parameter
+        self.assertEqual(User.select(id), 2)
+
+        # testing the post request for user without last_name parameter
         self.app.post('/users', data=dict(first_name='Jon',
-                                          last_name='Snow',
+                                          last_name=' ',
                                           email='jon+2@snow',
                                           password='toto1234'))
+        # testing without and email
+        self.app.post('/users', data=dict(first_name='Jon',
+                                          last_name='b ',
+                                          email=' ',
+                                          password='toto1234'))
+        self.assertEqual(User.select(id), 3)
 
+        # testing without a password
+        self.app.post('/users', data=dict(first_name='Jon',
+                                          last_name='b',
+                                          email='s@gmail.com ',
+                                          password=' '))
+        self.assertEqual(User.select(id), 4)
 
-        # check user id return should be 1
-        self.assertEqual(User.get().id, 1)
+        # testing if an user can't have the same email
+        email_test = self.app.post('/users', data=dict(first_name='Jon',
+                                                       last_name='Snow',
+                                                       email='jon@snow',
+                                                       password='toto1234'))
 
-        # check can user have the same email
+        assert email_test.status_code == 409
 
     # def test_list(self):
-    #    self.app.get('/users')
+    #    list_test = self.app.get('/users')
+    #    print list_test.id
