4174d4903b20b2975dfb2c103179e1c7966fc612
stvngrcia <stevengarciaacuna@gmail.com>
2016-07-27 18:49:06 -0700

    Created test for the creation of place

--- api/app/views/place.py
+++ api/app/views/place.py
@@ -5,8 +5,6 @@ from app.models.place import Place
 from app.models.base import db
 from flask import jsonify
 from flask import make_response
-import json
-from playhouse.shortcuts import model_to_dict
 
 
 @app.route('/places', methods=['GET', 'POST'])
@@ -15,10 +13,9 @@ def list_of_place():
     if request.method == 'GET':
         list = []
         for place in Place.select():
-            list.append(place.name)
-            # j = json.dumps(list)
-            parsed = json.loads()
-        return jsonify(parsed)
+            list.append(place.to_hash())
+        return jsonify(list)
+
     if request.method == 'POST':
         new_place = Place(owner=request.form['owner'],
                           city=request.form['city'],
@@ -32,66 +29,104 @@ def list_of_place():
                           longitude=request.form['longitude']
                           )
         new_place.save()
+        return "place created"
 
 
 @app.route('/places/<place_id>', methods=['GET', 'PUT', 'DELETE'])
 def modify_place(place_id):
     id = place_id
     if request.method == 'GET':
-        return Place.get(Place.id == id).to_hash()
+        try:
+            return jsonify(Place.get(Place.id == id).to_hash())
+        except:
+            return make_response(jsonify({'code': '10001',
+                                          'msg': 'no place found'}), 404)
+    elif request.method == 'PUT':
+        place = Place.select().where(Place.id == place_id).get()
+        params = request.values
+        for key in params:
+            if key == 'owner' or key == 'city':
+                return jsonify(msg="You may not update the %s." % key), 409
+            if key == 'updated_at' or key == 'created_at':
+                continue
+            else:
+                setattr(place, key, params.get(key))
+        place.save()
+        return jsonify(msg="Place information updated successfully."), 200
 
-    if request.method == 'PUT':
-        id = place_id
-        try:
-            # uptading name
-            query = Place.update(name=request.form['name']).where(Place.id == id)
-        except:
-            pass
-        try:
-            # updating description
-            query = Place.update(description=request.form['description']).where(Place.id == id)
-        except:
-            pass
-
-        try:
-            # updating user last_name
-            query = Place.update(number_rooms=request.form['number_rooms']).where(Place.id == id)
-        except:
-            pass
-        try:
-            # updating and securing the password
-            query = Place.update(number_bathrooms=request.form['number_bathrooms']).where(Place.id == id)
-        except:
-            pass
-        try:
-            query = Place.update(max_guest=request.form['max_guest']).where(Place.id == id)
-        except:
-            pass
-        try:
-            query = Place.update(price_by_night=request.form['price_by_night']).where(Place.id == id)
-        except:
-            pass
-        try:
-
-            query = Place.update(latitude=request.form['latitude']).where(Place.id == id)
-        except:
-            pass
-        try:
-
-            query = Place.update(longitude=request.form['longitude']).where(Place.id == id)
-        except:
-            pass
-            # returning message when trying to update owner or email
-        if request.form['owner']:
-            return "Owner can not be updated\n"
-        if request.form['city']:
-            return "City can not be updated\n"
-        query.execute()
-        return "updated\n"
-
-    if request.method == 'DELETE':
+    elif request.method == 'DELETE':
         try:
             get_place = Place.get(Place.id == id)
             get_place.delete_instance()
+            return "Place with id = %d was deleted\n" % (int(id))
         except:
-            return make_response(jsonify({'code': '10001', 'msg': 'no place found with that id'}), 409)
+            return make_response(jsonify({'code': '10001', 'msg': 'no place found with that id'}), 404)
+
+
+# def modify_place(place_id):
+#     try:
+#         id = place_id
+#         if request.method == 'GET':
+#             return jsonify(Place.get(Place.id == id).to_hash())
+#     except:
+#         return make_response(jsonify({'code': '10001',
+#                                       'msg': 'no place found'}), 404)
+#
+#     if request.method == 'PUT':
+#         id = place_id
+#         try:
+#             # uptading name
+#             query = Place.update(name=request.form['name']).where(Place.id == id)
+#             query.execute()
+#         except:
+#             pass
+#         try:
+#             # updating description
+#             query = Place.update(description=request.form['description']).where(Place.id == id)
+#             query.execute()
+#         except:
+#             pass
+#
+#         try:
+#             # updating user last_name
+#             query = Place.update(number_rooms=request.form['number_rooms']).where(Place.id == id)
+#             query.execute()
+#         except:
+#             pass
+#         try:
+#             # updating and securing the password
+#             query = Place.update(number_bathrooms=request.form['number_bathrooms']).where(Place.id == id)
+#             query.execute()
+#         except:
+#             pass
+#         try:
+#             query = Place.update(max_guest=request.form['max_guest']).where(Place.id == id)
+#             query.execute()
+#         except:
+#             pass
+#         try:
+#             query = Place.update(price_by_night=request.form['price_by_night']).where(Place.id == id)
+#             query.execute()
+#         except:
+#             pass
+#         try:
+#
+#             query = Place.update(latitude=request.form['latitude']).where(Place.id == id)
+#             query.execute()
+#         except:
+#             pass
+#         try:
+#
+#             query = Place.update(longitude=request.form['longitude']).where(Place.id == id)
+#             query.execute()
+#         except:
+#             pass
+#             # returning message when trying to update owner or city
+#         if request.form['owner']:
+#             return make_response(jsonify({'code': '10001',
+#                                           'msg': 'owner can not be updated'}), 409)
+#         elif request.form['city']:
+#             return make_response(jsonify({'code': '10001',
+#                                           'msg': 'city can not be updated'}), 409)
+#         query.execute()
+#         return "updated\n"
---
+++ api/tests/place.py
@@ -0,0 +1,134 @@
+from app import app
+import unittest
+import logging
+from app.models.base import db
+from app.models.place import Place
+from app.models.city import City
+from app.models.state import State
+from app.models.user import User
+from flask import jsonify
+
+class place_test(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+        # logging.disable(logging.CRITICAL)
+        # connectting to database
+        db.connect()
+        db.create_tables([User], safe=True)
+        db.create_tables([State], safe=True)
+        db.create_tables([City], safe=True)
+        db.create_tables([Place], safe=True)
+
+    def tearDown(self):
+        # deleting database
+        db.drop_tables([Place])
+        db.drop_tables([City])
+        db.drop_tables([User])
+        db.drop_tables([State])
+
+    def test_create(self):
+        # creating a new state depency of city
+        new_state = self.app.post('/states', data=dict(name='California'))
+        assert new_state.status_code == 200
+        # creating a new city
+        new_city = self.app.post('/states/1/cities', data=dict(name='San Francisco'))
+        assert new_city.status_code == 200
+        # creating a new user dependency for place
+        new_user = self.app.post('/users', data=dict(first_name='Jon',
+                                                     last_name='Snow',
+                                                     email='jon@snow',
+                                                     password='toto1234'))
+        assert new_user.status_code == 200
+        # creating a place
+        new_place = self.app.post('/places', data=dict(
+                                             owner=1,
+                                             city=1,
+                                             name="Steven",
+                                             description="house",
+                                             number_rooms=3,
+                                             number_bathrooms=2,
+                                             max_guest=3,
+                                             price_by_night=100,
+                                             latitude=37.774929,
+                                             longitude=-122.419416))
+        assert new_place.status_code == 200
+        # Getting place
+        get_place = self.app.get('/places')
+        # print get_place.data
+        assert get_place.status_code == 200
+
+    def test_get(self):
+        # creating a new state depency of city
+        new_state = self.app.post('/states', data=dict(name='California'))
+        assert new_state.status_code == 200
+        # creating a new city
+        new_city = self.app.post('/states/1/cities', data=dict(name='San Francisco'))
+        assert new_city.status_code == 200
+        # creating a new user dependency for place
+        new_user = self.app.post('/users', data=dict(first_name='Jon',
+                                                     last_name='Snow',
+                                                     email='jon@snow',
+                                                     password='toto1234'))
+        assert new_user.status_code == 200
+        # creating a place
+        new_place = self.app.post('/places', data=dict(
+                                             owner=1,
+                                             city=1,
+                                             name="Steven",
+                                             description="house",
+                                             number_rooms=3,
+                                             number_bathrooms=2,
+                                             max_guest=3,
+                                             price_by_night=100,
+                                             latitude=37.774929,
+                                             longitude=-122.419416))
+        assert new_place.status_code == 200
+        # creating a place 2
+        new_place2 = self.app.post('/places', data=dict(
+                                             owner=1,
+                                             city=1,
+                                             name="Steven apartment",
+                                             description="apartment",
+                                             number_rooms=1,
+                                             number_bathrooms=1,
+                                             max_guest=2,
+                                             price_by_night=100,
+                                             latitude=37.774929,
+                                             longitude=-122.419416))
+        assert new_place2.status_code == 200
+        # Getting place 2
+        get_place = self.app.get('/places/2')
+        assert get_place.status_code == 200
+        # Getting place 1
+        get_place = self.app.get('/places/1')
+        assert get_place.status_code == 200
+        # Getting place that does not exist
+        get_place = self.app.get('/places/3')
+        assert get_place.status_code == 404
+
+        # Trying to update place
+        new_update = self.app.put('/places/2', data=dict(city=2))
+        # print new_update.status_code
+        assert new_update.status_code == 409
+        new_update = self.app.put('/places/2', data=dict(owner=2))
+        # print new_update.data
+        assert new_update.status_code == 409
+        new_update = self.app.put('/places/2', data=dict(price_by_night=50))
+        # print new_update.data
+        assert new_update.status_code == 200
+        new_update = self.app.put('/places/2', data=dict(name='updated_data'))
+        assert new_update.status_code == 200
+        new_update = self.app.put('/places/2', data=dict(description='update'))
+        assert new_update.status_code == 200
+        new_update = self.app.put('/places/2', data=dict(max_guest=1))
+        assert new_update.status_code == 200
+        new_update = self.app.put('/places/2', data=dict(number_bathrooms=2))
+        assert new_update.status_code == 200
+        # deleting place
+        new_delete = self.app.delete('/places/1')
+        # print new_delete.status_code
+        assert new_delete.status_code == 200
+        # deleting place that does not exist
+        get_place = self.app.delete('/places/3')
+        assert get_place.status_code == 404
