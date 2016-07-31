8f55ac560fb23b8bf79336b10a8e742d600d8bfd
stvngrcia <stevengarciaacuna@gmail.com>
2016-07-31 14:46:28 -0700

    getting up to date

--- api/tests/review.py
+++ api/tests/review.py
@@ -110,69 +110,69 @@ class review_test(unittest.TestCase):
     #     assert len(get_review.data) > 0
 
     #
-    def test_delete(self):
-        # creating a review by an user that exist
-        new_review = self.app.post('/users/1/reviews',
-                                   data=dict(message="I like it",
-                                             user_id=1,
-                                             stars=5))
-        assert new_review.status_code == 200
-
-        # checking how many reviews there are before deleting
-        get_review = self.app.get('/users/1/reviews')
-        assert get_review.status_code == 200
-        size_before = len(json.loads(get_review.data))
-
-        # delition of a review that does not exist
-        deleting_review = self.app.delete('/users/1/reviews/3')
-        assert deleting_review.status_code == 404
-
-        # delition of a review that does not exist
-        deleting_review = self.app.delete('/users/13/reviews/1')
-        assert deleting_review.status_code == 404
-
-        # delition of a review that does exist
-        deleting_review = self.app.delete('/users/1/reviews/1')
-        assert deleting_review.status_code == 200
-        # checking how many reviews there are after deleting
-        get_review = self.app.get('/users/1/reviews')
-        assert get_review.status_code == 200
-        size_after = len(json.loads(get_review.data))
-        # checking that the size is smaller after deleting
-        assert size_after < size_before
-    #
-    # def test_review_place(self):
-    #     # Creating a setUp
-    #     new_state = State(name='California')
-    #     new_state.save()
-    #     new_city = City(name='San Francisco', state=1)
-    #     new_city.save()
-    #     new_place = Place(owner=1,
-    #                       city=1,
-    #                       name="Steven",
-    #                       description="house",
-    #                       number_rooms=3,
-    #                       number_bathrooms=2,
-    #                       max_guest=3,
-    #                       price_by_night=100,
-    #                       latitude=37.774929,
-    #                       longitude=-122.419416)
-    #     new_place.save()
-    #     new_review = self.app.post('/places/1/reviews',
+    # def test_delete(self):
+    #     # creating a review by an user that exist
+    #     new_review = self.app.post('/users/1/reviews',
     #                                data=dict(message="I like it",
     #                                          user_id=1,
     #                                          stars=5))
     #     assert new_review.status_code == 200
     #
-    #     # Getting a review for a place that does not exist
-    #     get_review_place = self.app.get('/places/3/reviews')
-    #     assert get_review_place == 404
+    #     # checking how many reviews there are before deleting
+    #     get_review = self.app.get('/users/1/reviews')
+    #     assert get_review.status_code == 200
+    #     size_before = len(json.loads(get_review.data))
     #
-    #     # Getting a review for a place that does exist
-    #     get_review_place = self.app.get('/places/1/reviews')
-    #     assert get_review_place == 200
-    #     # Checking that there is at least 1 review
-    #     assert len(get_review_place.data) > 0
+    #     # delition of a review that does not exist
+    #     deleting_review = self.app.delete('/users/1/reviews/3')
+    #     assert deleting_review.status_code == 404
+    #
+    #     # delition of a review that does not exist
+    #     deleting_review = self.app.delete('/users/13/reviews/1')
+    #     assert deleting_review.status_code == 404
+    #
+    #     # delition of a review that does exist
+    #     deleting_review = self.app.delete('/users/1/reviews/1')
+    #     assert deleting_review.status_code == 200
+    #     # checking how many reviews there are after deleting
+    #     get_review = self.app.get('/users/1/reviews')
+    #     assert get_review.status_code == 200
+    #     size_after = len(json.loads(get_review.data))
+    #     # checking that the size is smaller after deleting
+    #     assert size_after < size_before
+    #
+    def test_review_place(self):
+        # Creating a setUp
+        new_state = State(name='California')
+        new_state.save()
+        new_city = City(name='San Francisco', state=1)
+        new_city.save()
+        new_place = Place(owner=1,
+                          city=1,
+                          name="Steven",
+                          description="house",
+                          number_rooms=3,
+                          number_bathrooms=2,
+                          max_guest=3,
+                          price_by_night=100,
+                          latitude=37.774929,
+                          longitude=-122.419416)
+        new_place.save()
+        new_review = self.app.post('/places/1/reviews',
+                                   data=dict(message="I like it",
+                                             user_id=1,
+                                             stars=5))
+        assert new_review.status_code == 200
+
+        # Getting a review for a place that does not exist
+        get_review_place = self.app.get('/places/3/reviews')
+        assert get_review_place == 404
+
+        # Getting a review for a place that does exist
+        get_review_place = self.app.get('/places/1/reviews')
+        assert get_review_place == 200
+        # Checking that there is at least 1 review
+        assert len(get_review_place.data) > 0
     #
     # def test_review_place2(self):
     #     # Creating a setUp
