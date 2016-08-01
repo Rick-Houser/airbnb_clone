50bbaf94a696e4f8d538753960dd4d8f3a0c92f7
stvngrcia <stevengarciaacuna@gmail.com>
2016-08-01 10:27:57 -0700

    fixed indentation to match flake8

--- api/app/views/place_book.py
+++ api/app/views/place_book.py
@@ -3,8 +3,9 @@ from app.models.place_book import PlaceBook
 from flask import Flask, jsonify, request
 from datetime import datetime
 
-# List all books for a selected place
+
 @app.route('/places/<place_id>/books/', methods=["GET"])
+# List all books for a selected place
 def find_book(place_id):
 	try:
 		books = PlaceBook.select().where(PlaceBook.place == place_id)
@@ -13,7 +14,8 @@ def find_book(place_id):
 			book_list.append(book.to_dict())
 		return jsonify(book_list)
 	except:
-		return jsonify({'code': 404,'msg': 'not found'}), 404
+		return jsonify({'code': 404, 'msg': 'not found'}), 404
+
 
 # Create a new booking from POST data for a selected place
 @app.route('/places/<place_id>/books/', methods=["POST"])
@@ -21,34 +23,36 @@ def create_book(place_id):
 	try:
 		data = request.values
 		new_book = PlaceBook(
-			place = place_id,
-		 	user = data['user_id'],
-		 	date_start = datetime.strptime(data['date_start'], "%Y/%m/%d %H:%M:%S"),
-		 	number_nights = data['number_nights'],
-		 	is_validated = data['is_validated'])
+			place=place_id,
+			user=data['user_id'],
+			date_start=datetime.strptime(data['date_start'], "%Y/%m/%d %H:%M:%S"),
+			number_nights=data['number_nights'],
+			is_validated=data['is_validated'])
 		new_book.save()
 		return new_book.to_dict()
 	except:
-		return jsonify({'code': 404,'msg': 'not found'}), 404
+          return jsonify({'code': 404, 'msg': 'not found'}), 404
 
-# Find a booking
+
 @app.route('/places/<int:place_id>/books/<book_id>', methods=['GET'])
+# Find a booking
 def find_booking(place_id, book_id):
 	try:
 		booking = PlaceBook.get(PlaceBook.id == book_id)
 		return booking.to_dict()
 	except:
-		return jsonify({'code': 404,'msg': 'not found'}), 404
+		return jsonify({'code': 404, 'msg': 'not found'}), 404
 
-# Modify a booking
+
 @app.route('/places/<int:place_id>/books/<int:book_id>', methods=['PUT'])
+# Modify a booking
 def modify_booking(place_id, book_id):
 	try:
 		booking = PlaceBook.get(PlaceBook.id == book_id)
 		data = request.values()
 		for key in data:
 			if key == 'user':
-				return jsonify({'code': 405,'msg': 'Method not allowed'}), 405
+				return jsonify({'code': 405, 'msg': 'Method not allowed'}), 405
 			elif key == 'date_start':
 				booking.date_start = datetime.strptime(data[key], "%Y/%m/%d %H:%M:%S")
 			elif key == 'number_nights':
@@ -58,14 +62,15 @@ def modify_booking(place_id, book_id):
 		booking.save()
 		return booking.to_dict()
 	except:
-		return jsonify({'code': 404,'msg': 'not found'}), 404
+		return jsonify({'code': 404, 'msg': 'not found'}), 404
 
-# Delete a booking
+
 @app.route('/places/<int:place_id>/books/<int:book_id>', methods=['DELETE'])
+# Delete a booking
 def delete_booking(place_id, book_id):
 	try:
 		booking = PlaceBook.get(PlaceBook.id == book_id)
 		booking.delete_instance()
 		return jsonify({'msg': 'Deleted booking!'}), 200
 	except:
-		return jsonify({'code': 404,'msg': 'not found'}), 404
+		return jsonify({'code': 404, 'msg': 'not found'}), 404
