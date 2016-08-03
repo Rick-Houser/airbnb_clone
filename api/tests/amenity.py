386f00e6718a8b735cff20921a924167169865e6
stvngrcia <stevengarciaacuna@gmail.com>
2016-08-03 07:58:36 -0700

    Corrected intedentation, and use the correct variable names in some of the asserts

--- api/tests/amenity.py
+++ api/tests/amenity.py
@@ -6,38 +6,40 @@ import logging
 
 
 class AmenitiesTest(unittest.TestCase):
-	'''
-	Disable logging calls with levels less severe than or equal to CRITICAL.
-	connect to database and create table for testing.
-	'''
+    # Disable logging calls with levels less severe than or equal to CRITICAL.
+    # connect to database and create table for testing.
     def setUp(self):
-		logging.disable(logging.CRITICAL)
+        logging.disable(logging.CRITICAL)
         self.app = app.test_client()
-
         db.connect()
         db.create_table([Amenity], safe=True)
 
-	# Drop test table
-	def tearDown(self):
-		db.drop_table([Amenity], safe=True)
+        # Drop test table
+        def tearDown(self):
+            db.drop_table([Amenity], safe=True)
 
-	# Create new amenity
-    def test_post(self):
-        new_amenity = self.app.post('/amenities', data=dict(name='amenity1'))
-        assert new_amenity.status_code == 200
+        # Create new amenity
+        def test_post(self):
+            new_amenity = self.app.post('/amenities',
+                                        data=dict(name='amenity1'))
+            assert new_amenity.status_code == 200
 
-	# Get newly created amenity. 404 (Not Found) if amenity doesn't exist.
-	def test_get(self):
-		get_amenity = self.app.get('/amenities/1', data=dict(name='amenity1'))
-        assert new_amenity.status_code == 200
+        # Get newly created amenity. 404 (Not Found) if amenity doesn't exist.
+        def test_get(self):
+            get_amenity = self.app.get('/amenities/1',
+                                       data=dict(name='amenity1'))
+            assert get_amenity.status_code == 200
 
-		get_invalid_amenity = self.app.get('/amenities/9', data=dict(name='amenity1'))
-        assert new_amenity.status_code == 404
+            get_invalid_amenity = self.app.get('/amenities/9',
+                                               data=dict(name='amenity1'))
+            assert get_invalid_amenity.status_code == 404
 
-	# Delete newly created amenity. 404 (Not Found) if amenity doesn't exist.
-	def test_delete(self):
-		get_amenity = self.app.delete('/amenities/1', data=dict(name='amenity1'))
-        assert new_amenity.status_code == 200
+        # Delete newly created amenity.404 (Not Found) if amenity doesn't exist
+        def test_delete(self):
+            get_amenity = self.app.delete('/amenities/1',
+                                          data=dict(name='amenity1'))
+            assert get_amenity.status_code == 200
 
-		get_amenity = self.app.delete('/amenities/9', data=dict(name='amenity1'))
-        assert new_amenity.status_code == 404
+            get_amenity = self.app.delete('/amenities/9',
+                                          data=dict(name='amenity1'))
+            assert get_amenity.status_code == 404
