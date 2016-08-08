a596406266bddabb4ced1247a060abed374e9242
stvngrcia <stevengarciaacuna@gmail.com>
2016-08-07 20:58:13 -0700

    updated ListStyle to get the request values and check if there it is a page or a number, then paginate the json response

--- api/app/views/return_styles.py
+++ api/app/views/return_styles.py
@@ -1,10 +1,24 @@
-from flask import jsonify
+from flask import request
 
 
 class ListStyle():
     @staticmethod
     def list(select, request):
+
+        # Assigning a default values to page an number
+        number = 10
+        page = 1
+
+        # Traversing the parameters and checking if number and page
+        # exist in the URL of the request
+        for value in request.args:
+            if value == 'number':
+                number = int(request.values.get('number'))
+            elif value == 'page':
+                page = int(request.values.get('page'))
+
+        # creating a list for pagination
         list = []
-        for items in select:
+        for items in select.paginate(page, number):
             list.append(items.to_dict())
         return {'data': list}
