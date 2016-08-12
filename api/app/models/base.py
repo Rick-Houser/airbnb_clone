9fd9f7174bcbd9c1a25c9930c614b00fc6acdc5e
FrackenToaster <frakentoaster@gmail.com>
2016-08-12 13:10:18 -0700

    Add documentation for base.py

--- api/app/models/base.py
+++ api/app/models/base.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from config import *
 from datetime import datetime
 
-
+# Connectory to MySQL
 db = peewee.MySQLDatabase(DATABASE['database'],
                           host=DATABASE['host'],
                           port=DATABASE['port'],
@@ -12,6 +12,12 @@ db = peewee.MySQLDatabase(DATABASE['database'],
                           passwd=DATABASE['password'])
 
 
+'''
+ BaseModel:
+    - id: primary key
+    - created_at: datetime when a new resource is created
+    - updated_at: datetime when a resource is updated (via overloading save method)
+'''
 class BaseModel(peewee.Model):
     id = peewee.PrimaryKeyField(unique=True)
     updated_at = peewee.DateTimeField(default=datetime.now())
