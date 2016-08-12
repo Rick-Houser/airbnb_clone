edc0b168e33e3ce2a0710785f06e2b9aa72a6fba
FrackenToaster <frakentoaster@gmail.com>
2016-08-12 13:38:31 -0700

    Add documentation

--- api/app/models/place_amenity.py
+++ api/app/models/place_amenity.py
@@ -3,7 +3,11 @@ from amenity import *
 from place import *
 from base import *
 
-
+'''
+ PlaceAmenities:
+    - name: foreign key to Place
+    - amenity: foreign key to Amenity
+'''
 class PlaceAmenities(BaseModel):
     place = ForeignKeyField(Place)
     amenity = ForeignKeyField(Amenity)
