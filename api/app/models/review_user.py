d6f8e4f500a437d9e74d8083a31efb3512fd032c
FrackenToaster <frakentoaster@gmail.com>
2016-08-12 13:50:53 -0700

    Add documentation

--- api/app/models/review_user.py
+++ api/app/models/review_user.py
@@ -3,7 +3,11 @@ from peewee import *
 from user import User
 from review import Review
 
-
+'''
+ ReviewUser:
+    - user: foreign key to User
+    - review: foreign key to Review
+'''
 class ReviewUser(BaseModel):
     user = ForeignKeyField(User)
     review = ForeignKeyField(Review)
