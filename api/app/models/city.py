38f8f8b41a6839972cddd20ef7873a4a0c1f5fef
FrackenToaster <frakentoaster@gmail.com>
2016-08-12 13:13:10 -0700

    Add documentation for city model

--- api/app/models/city.py
+++ api/app/models/city.py
@@ -4,6 +4,11 @@ from state import *
 from datetime import datetime
 
 
+'''
+ City:
+    - name: not unique but cannot be blank
+    - state: foreign key to a state
+'''
 class City(BaseModel):
     name = CharField(128, null=False)
     state = ForeignKeyField(State, related_name='cities', on_delete="CASCADE")
