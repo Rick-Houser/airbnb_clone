06934c274cf4c99e4813dee35e4245e5465c2b01
FrackenToaster <frakentoaster@gmail.com>
2016-08-12 14:02:00 -0700

    Add documentation

--- api/app/models/user.py
+++ api/app/models/user.py
@@ -3,7 +3,14 @@ from peewee import *
 import md5
 from datetime import datetime
 
-
+'''
+ User:
+    - email: char field, cannot be blank and must be unique
+    - password: char field, cannot be blank
+    - first_name: char field, cannot be blank
+    - last_name: char field, cannot be blank
+    - is_admin: boolean, set to false by default
+'''
 class User(BaseModel):
     # unique so that there is not a copy in the database of email
     email = CharField(128, null=False, unique=True)
