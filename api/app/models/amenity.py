059420b9b39aae47d8a9a7d1543624ba2106a891
FrackenToaster <frakentoaster@gmail.com>
2016-08-12 12:57:04 -0700

    Add documentation

--- api/app/models/amenity.py
+++ api/app/models/amenity.py
@@ -2,12 +2,20 @@ from peewee import *
 from base import *
 from datetime import datetime
 
+'''
+Inherits from BaseModel and update configuration
+
+ Amenity:
+    - name: not unique but cannot be blank
+'''
+
 
 class Amenity(BaseModel):
     name = CharField(128, null=False)
 
     def to_dict(self):
-        return {'id': self.id,
+        return {
+                'id': self.id,
                 'created_at': self.created_at.strftime('%d/%m/%Y %H:%M:%S'),
                 'updated_at': self.updated_at.strftime('%d/%m/%Y %H:%M:%S'),
                 'name': self.name
