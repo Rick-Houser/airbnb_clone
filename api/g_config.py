6dc76646398111457b94b7b60ba866efe41df996
stvngrcia <stevengarciaacuna@gmail.com>
2016-08-16 02:34:04 +0000

    finished configuring gunicorn

--- api/g_config.py
+++ api/g_config.py
@@ -1,24 +1,25 @@
 import os
 
 name = os.environ.get('AIRBNB_ENV')
+print name
 
-
-if name == 'production':
+if name == "production":
     PORT = 3000
     errorlog = '/var/log/airbnb_api/error.log'
     accesslog = '/var/log/airbnb_api/access.log'
+    print "hello"
 
-
-elif name == 'test':
+elif name == "test":
     PORT = 5555
     errolog = '-'
     accesslog = '-'
 
 
-elif name == 'development':
+elif name == "development":
     PORT = 3333
     errolog = '-'
     accesslog = '-'
+# PORT = 3000
 
 bind = "0.0.0.0:" + str(PORT)
 reload = True
